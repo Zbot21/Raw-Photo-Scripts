de30cdeb963ec73069c66a3180189529019100c8
Chris <chrisfwin@gmail.com>
2018-01-06 08:04:01 -0800

    Raw photo management script. Removes all jpgs that are not also in raw format.

---
+++ remove_deleted_raw.pl
@@ -0,0 +1,16 @@
+my $directory = $ARGV[0];
+print $directory
+my %raw_files = map {$_ => 1} glob "$directory/*.{CR2,cr2}";
+my @jpg_files = glob "$directory/*.{JPG,jpg}";
+foreach $file (@jpg_files) {
+	print $file
+	$file =~ s/.(JPG|jpg)//;
+	if (!exists($raw_files{"$file.CR2"})) {
+		print "Deleting $file.CR2\n"
+		# unlink "$file.CR2"
+	}
+	if (!exists($raw_files{"$file.cr2"})) {
+		print "Deleting $file.cr2\n"
+		# unlink "$file.cr2"
+	}
+}
